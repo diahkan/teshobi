38ddd5723aadd1043204c215d7f9c5933769e4cc
diahkan <diahtriana12@gmail.com>
2024-11-12 21:27:10 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// Menampilkan notifikasi saat pesan dikirim
+document.getElementById("contact-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Pesan telah berhasil dikirim!");
+});
